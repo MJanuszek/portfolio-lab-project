0b7f11db87119377a441a9a405ee38fce3e34563
Monika Januszek <monika.januszek.mail@gmall.com>
2024-01-17 18:07:57 +0100

    StepsInfo component styled

--- src/components/StepsInfo.jsx
+++ src/components/StepsInfo.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import "../styles/StepsInfo.scss";
+import { Link } from "react-router-dom";
 function StepsInfo() {
   return (
     <>
       <div className="steps-info" id="steps-info">
         <h1>Wystarczą 4 proste kroki</h1>
-        <div className="decoration">-----dekoracja tu----</div>
+        <div className="decoration"></div>
         <div className="steps">
           <div className="step">
-            <span className="icon">---tu obrazek--</span>
-            <h4>tytuł kroku</h4>
-            <p>opis kroku</p>
+            <div className="step-icon-1"></div>
+            <h4 className="step-title">Wybierz rzeczy</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="step">
-            <span className="icon">---tu obrazek--</span>
-            <h4>tytuł kroku</h4>
-            <p>opis kroku</p>
+            <div className="step-icon-2"></div>
+            <h4 className="step-title">Spakuj je</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="step">
-            <span className="icon">---tu obrazek--</span>
-            <h4>tytuł kroku</h4>
-            <p>opis kroku</p>
+            <div className="step-icon-3"></div>
+            <h4 className="step-title">
+              Zdecyduj komu <br />
+              chcesz pomóc
+            </h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="step">
-            <span className="icon">---tu obrazek--</span>
-            <h4>tytuł kroku</h4>
-            <p>opis kroku</p>
+            <div className="step-icon-4"></div>
+            <h4 className="step-title">Zamów kuriera</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
-        <button className=" btn give-away">Oddaj rzeczy</button>
+        <Link to="/login" className="link-btn donate-link">
+          ODDAJ <br />
+          RZECZY
+        </Link>
       </div>
     </>
   );
