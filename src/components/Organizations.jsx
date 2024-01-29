28a92b4935efe7d12ace1e4a82db5cc05d8b8c53
Monika Januszek <monika.januszek.mail@gmall.com>
2024-01-29 15:29:30 +0100

    pagination added

--- src/components/Organizations.jsx
+++ src/components/Organizations.jsx
@@ -12,9 +12,12 @@ import {
 
 function Organizations() {
   const [displayCollection, setDisplayCollection] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(3);
   // collection: method that gives references to the collection from firebase
   const fundationsListRef = collection(database, "fundations-list");
   const organizationsListRef = collection(database, "organizations-list");
+  const fundraisingListRef = collection(database, "fundraising-list");
 
   const fetchCollectionData = async (collectionRef) => {
     const querySnapshot = await getDocs(collectionRef);
@@ -23,6 +26,7 @@ function Organizations() {
       ...doc.data(),
     }));
     setDisplayCollection(items);
+    setCurrentPage(1);
   };
 
   // change view of org list:::
@@ -34,47 +38,67 @@ function Organizations() {
     } else if (id === "organizations") {
       fetchCollectionData(organizationsListRef);
     } else if (id === "collections") {
-      setDisplayCollection("collections");
+      fetchCollectionData(fundraisingListRef);
     }
   };
+
+  // Oblicz indeksy elementów dla bieżącej strony
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = displayCollection.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  // Zmień stronę
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <>
       <div className="organizations" id="organizations">
         <h1>Komu Pomagamy?</h1>
+
         <div className="org-decoration"></div>
-        <button
-          className="fundations"
-          id="fundations"
-          onClick={() => {
-            handleDisplayList("fundations");
-          }}
-        >
-          Fundacjom
-        </button>
-        {/* ----------------- */}
-        <button
-          className="fundations"
-          id="organizations"
-          onClick={() => {
-            handleDisplayList("organizations");
-          }}
-        >
-          Organizacjom pozarządowym
-        </button>
-        {/* ------------------ */}
-        <button
-          className="fundations"
-          id="collections"
-          onClick={() => {
-            handleDisplayList("collections");
-          }}
-        >
-          Lokalnym zbórkom
-        </button>
+        <div className="buttons">
+          <button
+            className="fundations-btn display-btn"
+            id="fundations"
+            onClick={() => {
+              handleDisplayList("fundations");
+            }}
+          >
+            Fundacjom
+          </button>
+          {/* ----------------- */}
+          <button
+            className="organizations-btn display-btn"
+            id="organizations"
+            onClick={() => {
+              handleDisplayList("organizations");
+            }}
+          >
+            Organizacjom pozarządowym
+          </button>
+          {/* ------------------ */}
+          <button
+            className="collections-btn display-btn"
+            id="collections"
+            onClick={() => {
+              handleDisplayList("collections");
+            }}
+          >
+            Lokalnym zbórkom
+          </button>
+        </div>
+        {/* ----------- */}
+        <p className="list-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse dolor,
+          fugiat laudantium fuga velit porro. Quo eligendi optio, quas dolorem
+          labore nostrum!
+        </p>
         {/* -------------------- */}
       </div>
       <ul>
-        {displayCollection.map((element, index) => {
+        {currentItems.map((element, index) => {
           return (
             <ListElement
               className="single-element"
@@ -85,6 +109,16 @@ function Organizations() {
           );
         })}
       </ul>
+      <div className="pagination">
+        {Array.from(
+          { length: Math.ceil(displayCollection.length / itemsPerPage) },
+          (_, index) => (
+            <button key={index} onClick={() => paginate(index + 1)}>
+              {index + 1}
+            </button>
+          )
+        )}
+      </div>
     </>
   );
 }
